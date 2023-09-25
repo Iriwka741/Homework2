2e7b4a0ae97432a3fb4b72212452b93740631ca3
Lenovo <katashirina89@gmail.com>
2023-09-25 19:38:53 +0300

    Implemented first task point three

--- main.py
+++ main.py
@@ -15,15 +15,22 @@
 #else:
     #print("All numbers equals")
 ###################################
+#number1 = int(input("Enter first number: "))
+#number2 = int(input("Enter second number: "))
+#number3 = int(input("Enter third number: "))
+
+#if number1 < number2 < number3:
+    #print(number1)
+#elif number2 < number1 < number3:
+    #print(number2)
+#elif number3 < number2 < number1:
+    #print(number3)
+#else:
+   # print("All numbers equals")
+   #################################
 number1 = int(input("Enter first number: "))
 number2 = int(input("Enter second number: "))
 number3 = int(input("Enter third number: "))
 
-if number1 < number2 < number3:
-    print(number1)
-elif number2 < number1 < number3:
-    print(number2)
-elif number3 < number2 < number1:
-    print(number3)
-else:
-    print("All numbers equals")
+result = (number1 + number2 + number3)//3
+print(f"Result: {result}")
