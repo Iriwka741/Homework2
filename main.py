9d82ba3c78655bc0038d19b8d95ac575db8b8913
Lenovo <katashirina89@gmail.com>
2023-09-25 19:31:31 +0300

    Implemented first task point one

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
 #print("Hello teacher")
 
-print("homework Banul ")
+#print("homework Banul ")
+
+number1 = int(input("Enter first number: "))
+number2 = int(input("Enter second number: "))
+number3 = int(input("Enter third number: "))
+
+if number1 > number2 > number3:
+    print(number1)
+elif number2 > number1 > number3:
+    print(number2)
+elif number3 > number2 > number1:
+    print(number3)
+else:
+    print("All numbers equals")
