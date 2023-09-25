fbc830f98a3a2dd50da040c5f6e0b671f1c5b710
Lenovo <katashirina89@gmail.com>
2023-09-25 19:36:31 +0300

    Implemented first task point two

--- main.py
+++ main.py
@@ -2,15 +2,28 @@
 
 #print("homework Banul ")
 
+#number1 = int(input("Enter first number: "))
+#number2: int = int(input("Enter second number: "))
+#number3 = int(input("Enter third number: "))
+
+#if number1 > number2 > number3:
+    #print(number1)
+#elif number2 > number1 > number3:
+    #print(number2)
+#elif number3 > number2 > number1:
+    #print(number3)
+#else:
+    #print("All numbers equals")
+###################################
 number1 = int(input("Enter first number: "))
 number2 = int(input("Enter second number: "))
 number3 = int(input("Enter third number: "))
 
-if number1 > number2 > number3:
+if number1 < number2 < number3:
     print(number1)
-elif number2 > number1 > number3:
+elif number2 < number1 < number3:
     print(number2)
-elif number3 > number2 > number1:
+elif number3 < number2 < number1:
     print(number3)
 else:
     print("All numbers equals")
